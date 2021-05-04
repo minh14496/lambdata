b25732d8577725c8bb7328d3adc540b8b350f758
Minh Nguyen <minh14496@gmail.com>
2021-05-03 17:13:39 -0700

    change the gitignore file

--- lambdata_minh/helper_functions.py
+++ lambdata_minh/helper_functions.py
@@ -1,14 +1,25 @@
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame()
-
 def null_count(df):
     """
     Check a dataframe for nulls and return the number of missing values.
 
     Args:
         df ([type]): pandas dataframe
+    Example Input (df = pd.DataFrame):
+    > | column 0    | column 1    | column 2    |
+    > | ----------- | ----------- | ----------- |
+    > | NaN         | 9           | 10          |
+    > | 4           | NaN         | 2           |
+    > | 3           | NaN         | 2           |
+
+    Function:
+    > `null_count(df)`
+
+    Expected Output (int):
+    > `3`
+
     """
     return df.isna().sum().sum()
 
@@ -19,12 +30,36 @@ def train_test_split(df, frac):
     Args:
         df ([type]): pandas dataframe
         frac ([type]): referes to the precent of data you would like to set aside for training.
+
+    Example Input (df = pd.DataFrame):
+    > | column 0    | column 1    | column 2    |
+    > | ----------- | ----------- | ----------- |
+    > | 0           | 1           | 2           |
+    > | 3           | 4           | 5           |
+    > | 6           | 7           | 8           |
+
+    Function:
+    > `train_test_split(df, frac=0.2)`
+
+    Expected Output (tuple of two dataframes):
+        (
+    > | column 0    | column 1    | column 2    | 
+    > | ----------- | ----------- | ----------- |
+    > | 3           | 4           | 5           |
+        ,
+    > | column 0    | column 1    | column 2    | 
+    > | ----------- | ----------- | ----------- | 
+    > | 0           | 1           | 2           |
+    > | 6           | 7           | 8           |
+        )
+
     """
-    assert type(frac)==float
-    length = len(df)*frac
+    #assert type(frac)==float
+    length = int(len(df)*frac)
     df1 = df[:length].copy()
     df2 = df[length:].copy()
     return (df1,df2)
+
 def randomize(df, seed):
     """
     Develop a randomization function that randomizes all of a dataframes cells then returns that randomized dataframe. 
@@ -32,6 +67,55 @@ def randomize(df, seed):
     Args:
         df ([type]): pandas dataframe
         seed ([type]): an int input. Random seed for reproducible randomization
+
+    Example Input (df = pd.DataFrame):
+    > | column 0    | column 1    | column 2    |
+    > | ----------- | ----------- | ----------- |
+    > | 0           | 1           | 2           |
+    > | 3           | 4           | 5           |
+    > | 6           | 7           | 8           |
+
+    Function:
+    > `randomize(df, seed=101)`
+
+    Expected Output (pd.Dataframe): 
+    > | column 0    | column 1    | column 2    |
+    > | ----------- | ----------- | ----------- |
+    > | 2           | 5           | 8           |
+    > | 0           | 3           | 6           |
+    > | 1           | 4           | 7           |
+
     """
     df = df.sample(frac=1,random_state=seed)
     return df
+
+# def addy_split(addy_series):
+#     """
+#     Split addresses into three columns (df['city'], df['state'], and df['zip']).
+    
+#     Args:
+#         addy_series ([type]): pandas Series
+
+#     Example Input (addy_series = pd.Series):
+#     > | address                                    |
+#     > | ------------------------------------------ |
+#     > | 890 Jennifer Brooks\nNorth Janet, WY 24785 |
+#     > | 8394 Kim Meadow\nDarrenville, AK 27389     |
+#     > | 379 Cain Plaza\nJosephburgh, WY 06332      |
+#     > | 5303 Tina Hill\nAudreychester, VA 97036    |
+
+#     Function:
+#     > `addy_split(addy_series)`
+
+#     Expected Output (pd.Dataframe): 
+#     > | city          | state       | zip         |
+#     > | ------------- | ----------- | ----------- |
+#     > | North Janet   | WY          | 24785       |
+#     > | Darrenville   | AK          | 27389       |
+#     > | Josephburgh   | WY          | 06332       |
+#     > | Audreychester | VA          | 97036       |
+
+#     """
+#     return addy_series
+
+    
